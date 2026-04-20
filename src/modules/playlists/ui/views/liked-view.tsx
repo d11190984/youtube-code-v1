7eded795ef5bbc9f6a383549dad571afe5856ad1
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 19:11:59 +0700

    Update liked-view.tsx

--- src/modules/playlists/ui/views/liked-view.tsx
+++ src/modules/playlists/ui/views/liked-view.tsx
@@ -1,12 +1,14 @@
+"use client";
+
 import { LikedVideosSection } from "../sections/liked-videos-section";
 
 export const LikedView = () => {
   return (
-    <div className="max-w-screen-md mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+    <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
       <div>
-        <h1 className="text-2xl font-bold">Liked</h1>
+        <h1 className="text-2xl font-bold">Video đã thích</h1>
         <p className="text-xs text-muted-foreground">
-          Videos you have liked
+          Những video bạn đã thích
         </p>
       </div>
       <LikedVideosSection />
