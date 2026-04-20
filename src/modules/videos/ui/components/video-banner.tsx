ee374a3db16b3f9836174457fa7e75f506cd93f6
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 20:10:13 +0700

    Update video-banner.tsx

--- src/modules/videos/ui/components/video-banner.tsx
+++ src/modules/videos/ui/components/video-banner.tsx
@@ -13,7 +13,7 @@ export const VideoBanner = ({ status }: VideoBannerProps) => {
     <div className="bg-yellow-500 py-3 px-4 rounded-b-xl flex items-center gap-2">
       <AlertTriangleIcon className="size-4 text-black shrink-0" />
       <p className="text-xs md:text-sm font-medium text-black line-clamp-1">
-        This video is still being processed.
+        Video đang được xử lý.
       </p>
     </div>
   );
