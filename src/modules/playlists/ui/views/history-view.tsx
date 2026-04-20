a0676735488baa4621efa911e779794e9ad49464
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 19:12:20 +0700

    Update history-view.tsx

--- src/modules/playlists/ui/views/history-view.tsx
+++ src/modules/playlists/ui/views/history-view.tsx
@@ -1,12 +1,14 @@
+"use client";
+
 import { HistoryVideosSection } from "../sections/history-videos-section";
 
 export const HistoryView = () => {
   return (
-    <div className="max-w-screen-md mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+    <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
       <div>
-        <h1 className="text-2xl font-bold">History</h1>
+        <h1 className="text-2xl font-bold">Lịch sử</h1>
         <p className="text-xs text-muted-foreground">
-          Videos you have watched
+          Những video bạn đã xem
         </p>
       </div>
       <HistoryVideosSection />
