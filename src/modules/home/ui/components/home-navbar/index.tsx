bbd1199f22c5d39acc7548698cb645b518017a8e
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 19:46:52 +0700

    Update index.tsx

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -16,8 +16,8 @@ export const HomeNavbar = () => {
           <SidebarTrigger />
           <Link prefetch href="/" className="hidden md:block">
             <div className="p-4 flex items-center gap-1">
-              <Image src="/logo.svg" alt="Logo" width={32} height={32} />
-              <p className="text-xl font-semibold tracking-tight">NewTube</p>
+              <Image src="/yuuka.png" alt="Logo" width={32} height={32} />
+              <p className="text-xl font-semibold tracking-tight">Hayase Yuuka</p>
             </div>
           </Link>
         </div>
