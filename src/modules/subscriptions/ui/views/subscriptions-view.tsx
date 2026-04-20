f9f2655dbfdab4b57297f8cbbb2b19eb5916bccd
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 19:17:50 +0700

    Update subscriptions-view.tsx

--- src/modules/subscriptions/ui/views/subscriptions-view.tsx
+++ src/modules/subscriptions/ui/views/subscriptions-view.tsx
@@ -1,12 +1,14 @@
+"use client";
+
 import { SubscriptionsSection } from "../sections/subscriptions-section";
 
 export const SubscriptionsView = () => {
   return (
-    <div className="max-w-screen-md mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+    <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
       <div>
-        <h1 className="text-2xl font-bold">All subscriptions</h1>
+        <h1 className="text-2xl font-bold">Tất cả kênh đã đăng ký</h1>
         <p className="text-xs text-muted-foreground">
-          View and manage all your subscriptions
+          Xem và quản lý tất cả kênh bạn đã đăng ký
         </p>
       </div>
       <SubscriptionsSection />
