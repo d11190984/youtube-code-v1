f55b4e1258c8fa7958a4dc178933fce359ac6385
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 19:50:11 +0700

    Update search-input.tsx

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -52,7 +52,7 @@ const SearchInputSuspense = () => {
           value={value}
           onChange={(e) => setValue(e.target.value)}
           type="text"
-          placeholder="Search"
+          placeholder="Tìm kiếm"
           className="w-full pl-4 py-2 pr-12 rounded-l-full border focus:outline-none focus:border-blue-500"
         />
         {value && (
