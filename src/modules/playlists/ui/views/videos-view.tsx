2d57355c3c67336786b072f90bd4d4e0c2d470c6
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-22 19:40:33 +0700

    Update videos-view.tsx

--- src/modules/playlists/ui/views/videos-view.tsx
+++ src/modules/playlists/ui/views/videos-view.tsx
@@ -7,7 +7,7 @@ interface VideosViewProps {
 
 export const VideosView = ({ playlistId }: VideosViewProps) => {
   return (
-    <div className="max-w-screen-md mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+    <div className="max-w-[2400px] mx-auto mb-10 px-4 flex flex-col gap-y-6">
       <PlaylistHeaderSection playlistId={playlistId} />
       <VideosSection playlistId={playlistId} />
     </div>
