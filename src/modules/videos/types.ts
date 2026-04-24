51667f3a421363df24d0fa03ca1cabf86f5a49fc
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-24 16:52:48 +0700

    Update types.ts

--- src/modules/videos/types.ts
+++ src/modules/videos/types.ts
@@ -1,10 +1,14 @@
 import { inferRouterOutputs } from "@trpc/server";
-
 import { AppRouter } from "@/trpc/routers/_app";
 
+// Lấy output của videos.getOne
 export type VideoGetOneOutput = 
   inferRouterOutputs<AppRouter>["videos"]["getOne"];
-  
-// TODO: Change to videos getMany
+
+// Lấy output của suggestions.getMany (giữ nguyên)
 export type VideoGetManyOutput = 
   inferRouterOutputs<AppRouter>["suggestions"]["getMany"];
+
+// Nếu muốn dùng trong VideoGridCard với progress:
+// Chỉ cần lấy một item từ suggestions:
+export type VideoGetManyItem = VideoGetManyOutput["items"][number];
