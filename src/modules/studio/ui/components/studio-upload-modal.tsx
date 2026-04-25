377b2cfd96e9a54e9e58d45292c628be1af9f24d
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-25 15:52:27 +0700

    Update studio-upload-modal.tsx

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -2,10 +2,10 @@
 
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
-import { Loader2Icon, PlusIcon } from "lucide-react";
+import { Loader2Icon, PlusIcon } from "lucide-react"
 
 import { trpc } from "@/trpc/client";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 import { ResponsiveModal } from "@/components/responsive-modal";
 
 import { StudioUploader } from "./studio-uploader";
@@ -13,57 +13,38 @@ import { StudioUploader } from "./studio-uploader";
 export const StudioUploadModal = () => {
   const router = useRouter();
   const utils = trpc.useUtils();
-
   const create = trpc.videos.create.useMutation({
     onSuccess: () => {
-      toast.success("Video đang được tạo, bạn có thể bắt đầu upload ✨");
+      toast.success("Video đang được tạo, bạn có thể bắt đầu upload");
       utils.studio.getMany.invalidate();
     },
-    onError: (err) => {
-      console.error(err);
-      toast.error(
-        "Đã xảy ra lỗi. Có thể đã đạt giới hạn video trên Mux hoặc lỗi server. Xóa video cũ hoặc liên hệ support nha.",
-      );
+    onError: () => {
+      toast.error("Đã xảy ra lỗi vì đủ 10 video trên Mux nên không thể tạo video mới. Vui lòng xóa bớt video cũ hoặc liên hệ support để được hỗ trợ.");
     },
   });
 
-  const handleSuccess = () => {
+  const onSuccess = () => {
     if (!create.data?.video.id) return;
 
     create.reset();
     router.push(`/studio/videos/${create.data.video.id}`);
   };
 
-  const isOpen = !!create.data?.url;
-
   return (
     <>
       <ResponsiveModal
         title="Upload a video"
-        open={isOpen}
+        open={!!create.data?.url}
         onOpenChange={() => create.reset()}
       >
-        {isOpen ? (
-          <StudioUploader
-            endpoint={create.data?.url || ""}
-            onSuccess={handleSuccess}
-          />
-        ) : (
-          <Loader2Icon className="animate-spin text-muted-foreground mx-auto my-12" />
-        )}
+        {create.data?.url 
+          ? <StudioUploader endpoint={create.data.url} onSuccess={onSuccess} /> 
+          : <Loader2Icon />
+        }
       </ResponsiveModal>
-
-      <Button
-        variant="secondary"
-        onClick={() => create.mutate()}
-        disabled={create.isPending}
-      >
-        {create.isPending ? (
-          <Loader2Icon className="animate-spin mr-2 h-4 w-4" />
-        ) : (
-          <PlusIcon className="mr-2 h-4 w-4" />
-        )}
-        Tạo
+      <Button variant="secondary" onClick={() => create.mutate()} disabled={create.isPending}>
+        {create.isPending ? <Loader2Icon className="animate-spin" /> : <PlusIcon />}
+       Tạo
       </Button>
     </>
   );
