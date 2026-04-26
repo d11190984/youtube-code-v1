cb29deceec1f335599cec0dcc2f0c5175646981d
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-26 10:41:35 +0700

    Update user-view.tsx

--- src/modules/users/ui/views/user-view.tsx
+++ src/modules/users/ui/views/user-view.tsx
@@ -1,5 +1,7 @@
+// UserView.tsx
 "use client";
 
+import { useState } from "react";
 import { UserSection } from "../sections/user-section";
 import { VideosSection } from "../sections/videos-section";
 import { UserTabs } from "../components/user-tabs";
@@ -9,11 +11,19 @@ interface UserViewProps {
 }
 
 export const UserView = ({ userId }: UserViewProps) => {
+  const [activeVideoTab, setActiveVideoTab] = useState<
+    "latest" | "popular" | "oldest"
+  >("latest");
+
   return (
     <div className="flex flex-col max-w-[1300px] px-4 pt-2.5 mx-auto mb-10 gap-y-6">
       <UserSection userId={userId} />
-      <UserTabs userId={userId} />
-      <VideosSection userId={userId} /> {/* sort mặc định latest */}
+      <UserTabs
+        userId={userId}
+        activeVideoTab={activeVideoTab}
+        setActiveVideoTabAction={setActiveVideoTab} // ✅ trùng với interface
+      />
+      <VideosSection userId={userId} sortBy={activeVideoTab} />
     </div>
   );
 };
