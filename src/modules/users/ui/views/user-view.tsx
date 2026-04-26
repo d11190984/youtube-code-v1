4cc850c49a29603ed9887df32457da90e0d079ec
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-26 09:25:23 +0700

    Update user-view.tsx

--- src/modules/users/ui/views/user-view.tsx
+++ src/modules/users/ui/views/user-view.tsx
@@ -1,15 +1,19 @@
+"use client";
+
 import { UserSection } from "../sections/user-section";
 import { VideosSection } from "../sections/videos-section";
+import { UserTabs } from "../components/user-tabs";
 
 interface UserViewProps {
   userId: string;
-};
+}
 
 export const UserView = ({ userId }: UserViewProps) => {
   return (
     <div className="flex flex-col max-w-[1300px] px-4 pt-2.5 mx-auto mb-10 gap-y-6">
       <UserSection userId={userId} />
-      <VideosSection userId={userId} />
+      <UserTabs userId={userId} />
+      <VideosSection userId={userId} /> {/* sort mặc định latest */}
     </div>
   );
 };
