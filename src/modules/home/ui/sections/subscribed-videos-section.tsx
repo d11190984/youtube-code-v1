517dfe82acbf44fb161d20db11c0bc236af04c32
Manhwa-Academy <vuliztva1@gmail.com>
2026-05-11 14:48:41 +0700

    a

--- src/modules/home/ui/sections/subscribed-videos-section.tsx
+++ src/modules/home/ui/sections/subscribed-videos-section.tsx
@@ -8,7 +8,10 @@ import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
 import { InfiniteScroll } from "@/components/infinite-scroll";
 
-import { VideoGridCard, VideoGridCardSkeleton } from "@/modules/videos/ui/components/video-grid-card";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
 
 export const SubscribedVideosSection = () => {
   return (
@@ -23,34 +26,38 @@ export const SubscribedVideosSection = () => {
 const SubscribedVideosSectionSkeleton = () => {
   return (
     <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5 [@media(min-width:2200px)]:grid-cols-6">
-        {Array.from({ length: 18 }).map((_, index) => (
-            <VideoGridCardSkeleton key={index} />
-          ))
-        }
-      </div>
-  )
-}
+      {Array.from({ length: 18 }).map((_, index) => (
+        <VideoGridCardSkeleton key={index} />
+      ))}
+    </div>
+  );
+};
 
 const SubscribedVideosSectionSuspense = () => {
-  const query = trpc.videos.getManySubscribed.useSuspenseInfiniteQuery(
-    { limit: DEFAULT_LIMIT },
-    {
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    }
-  );
-
-  const videos = query.data;
+  const [videos, query] =
+    trpc.videos.getManySubscribed.useSuspenseInfiniteQuery(
+      { limit: DEFAULT_LIMIT },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      },
+    );
 
   return (
     <div>
       <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5 [@media(min-width:2200px)]:grid-cols-6">
         {videos.pages
           .flatMap((page) => page.items)
-          .filter((video) => !(video.videoHeight && video.videoWidth && video.videoHeight > video.videoWidth))
+          .filter(
+            (video) =>
+              !(
+                video.videoHeight &&
+                video.videoWidth &&
+                video.videoHeight > video.videoWidth
+              ),
+          )
           .map((video) => (
             <VideoGridCard key={video.id} data={video} />
-          ))
-        }
+          ))}
       </div>
       <InfiniteScroll
         hasNextPage={query.hasNextPage}
@@ -58,5 +65,5 @@ const SubscribedVideosSectionSuspense = () => {
         fetchNextPage={query.fetchNextPage}
       />
     </div>
-  )
-}
+  );
+};
